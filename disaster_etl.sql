3fe7bd53f6a55e7bc180ce487cf1db7210ca6048
Chris Howard <survifit@users.noreply.github.com>
2019-05-18 08:21:09 -0500

    Cleaned up UFO table, created connection for first table load, started report

--- disaster_etl.sql
+++ disaster_etl.sql
@@ -1,3 +1,13 @@
-create database disaster_etl;
+create database if not exists disaster_etl;
 
 use disaster_etl;
+
+create table if not exists ufo_sightings (
+	id int primary key auto_increment,
+    year text,
+    month text,
+    day text,
+    date text,
+    name text, 
+    country text,
+    description text)
