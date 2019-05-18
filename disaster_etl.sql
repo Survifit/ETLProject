6884f9f30858a6caa8e7a2d8eb2c9586fbe472c4
Purple Instance <47543437+snhartje@users.noreply.github.com>
2019-05-18 12:03:10 -0500

    add volcano table, format Lat Lon, and add SQL code

--- disaster_etl.sql
+++ disaster_etl.sql
@@ -10,4 +10,27 @@ create table if not exists ufo_sightings (
     date text,
     name text, 
     country text,
-    description text)
+    description text);
+    
+create table if not exists Volcano_Eruptions (
+	id int primary key auto_increment,
+    Year text,
+    Month text,
+    Day text,
+    Volcano_ID text,
+    Volcano_Name text,
+    Location text,
+    Latitude text,
+    Longitude text
+);
+
+create table if not exists Hurricanes(
+	Year text,
+    Month text,
+    Day text,
+    ID text,
+    Latitude text,
+    Longitude text
+);
+ 
+ 
